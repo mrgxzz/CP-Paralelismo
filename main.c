72e758ceedc0e47880de7b21cd74fa5f6e6fcc71
mrgxzz <manuel.romeo@udc.es>
2024-04-16 11:53:42 +0200

    Changes by raulas

--- main.c
+++ main.c
@@ -1,27 +1,78 @@
 #include <stdio.h>
 #include <math.h>
+#include <mpi.h>
 
-int main(int argc, char *argv[])
-{
-    int i, done = 0, n;
+// Definición de MPI_FlattreeColectiva para la recolección de sum
+void MPI_FlattreeColectiva(double *mypi, double *pi, int numprocs, int myid) {
+    MPI_Status status;
+    int mask = 1;
+    while (mask < numprocs) {
+        if ((myid & mask) == 0) {
+            int partner = myid | mask;
+            if (partner < numprocs) {
+                double recv_pi;
+                MPI_Recv(&recv_pi, 1, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD, &status);
+                *pi += recv_pi;
+            }
+        } else {
+            int partner = myid & (~mask);
+            MPI_Send(mypi, 1, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD);
+        }
+        mask <<= 1;
+    }
+}
+
+// Definición de MPI_BinomialColectiva para la distribución de n
+void MPI_BinomialColectiva(int *n, int numprocs, int myid) {
+    int mask = 1;
+    while (mask < numprocs) {
+        int partner = myid ^ mask;
+        if (partner < numprocs) {
+            if (myid & mask) {
+                MPI_Recv(n, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            } else {
+                MPI_Send(n, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+            }
+        }
+        mask <<= 1;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int i, n, numprocs, myid;
     double PI25DT = 3.141592653589793238462643;
-    double pi, h, sum, x;
+    double pi, h, sum, x, mypi;
 
-    while (!done)
-    {
-        printf("Enter the number of intervals: (0 quits) \n");
-        scanf("%d",&n);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    while (1) {
+        if (myid == 0) {
+            printf("Enter the number of intervals: (0 quits) \n");
+            scanf("%d", &n);
+        }
+        MPI_BinomialColectiva(&n, numprocs, myid);
 
         if (n == 0) break;
 
-        h   = 1.0 / (double) n;
+        h = 1.0 / (double)n;
         sum = 0.0;
-        for (i = 1; i <= n; i++) {
+        for (i = myid + 1; i <= n; i += numprocs) {
             x = h * ((double)i - 0.5);
-            sum += 4.0 / (1.0 + x*x);
+            sum += 4.0 / (1.0 + x * x);
         }
-        pi = h * sum;
+        mypi = h * sum;
 
-        printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        // Utilizar MPI_FlattreeColectiva para recolectar la estimación de PI de cada proceso
+        pi = 0.0;
+        MPI_FlattreeColectiva(&mypi, &pi, numprocs, myid);
+
+        if (myid == 0) {
+            printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        }
     }
+
+    MPI_Finalize();
+    return 0;
 }
