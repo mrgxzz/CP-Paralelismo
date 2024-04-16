7ddc77be9ad0dda0d3244889cb1c396ee3966c48
mrgxzz <manuel.romeo@udc.es>
2024-04-16 11:59:44 +0200

    p2 done

--- main.c
+++ main.c
@@ -1,78 +1,80 @@
+
 #include <stdio.h>
 #include <math.h>
 #include <mpi.h>
 
-// Definición de MPI_FlattreeColectiva para la recolección de sum.
-void MPI_FlattreeColectiva(double *mypi, double *pi, int numprocs, int myid) {
-    MPI_Status status;
-    int mask = 1;
-    while (mask < numprocs) {
-        if ((myid & mask) == 0) {
-            int partner = myid | mask;
-            if (partner < numprocs) {
-                double recv_pi;
-                MPI_Recv(&recv_pi, 1, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD, &status);
-                *pi += recv_pi;
-            }
-        } else {
-            int partner = myid & (~mask);
-            MPI_Send(mypi, 1, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD);
-        }
-        mask <<= 1;
-    }
-}
+void MPI_FlattreeColectiva(void * buff, void * recvbuff, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm);
+void MPI_BinomialColectiva(void* buff, int count, MPI_Datatype datatype, int root, MPI_Comm comm);
 
-// Definición de MPI_BinomialColectiva para la distribución de n
-void MPI_BinomialColectiva(int *n, int numprocs, int myid) {
-    int mask = 1;
-    while (mask < numprocs) {
-        int partner = myid ^ mask;
-        if (partner < numprocs) {
-            if (myid & mask) {
-                MPI_Recv(n, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            } else {
-                MPI_Send(n, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
-            }
-        }
-        mask <<= 1;
-    }
-}
-
-int main(int argc, char *argv[]) {
-    int i, n, numprocs, myid;
+int main(int argc, char *argv[]){
+    int i, done = 0, n;
     double PI25DT = 3.141592653589793238462643;
-    double pi, h, sum, x, mypi;
+    double pi, h, sum, x;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    int rank, numprocs;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    while (1) {
-        if (myid == 0) {
+    while (!done) {
+        if (rank == 0) {
             printf("Enter the number of intervals: (0 quits) \n");
             scanf("%d", &n);
         }
-        MPI_BinomialColectiva(&n, numprocs, myid);
-
+        MPI_BinomialColectiva(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
         if (n == 0) break;
 
-        h = 1.0 / (double)n;
+        h = 1.0 / (double) n;
         sum = 0.0;
-        for (i = myid + 1; i <= n; i += numprocs) {
-            x = h * ((double)i - 0.5);
+        for (i = rank + 1; i <= n; i += numprocs) {
+            x = h * ((double) i - 0.5);
             sum += 4.0 / (1.0 + x * x);
         }
-        mypi = h * sum;
-
-        // Utilizar MPI_FlattreeColectiva para recolectar la estimación de PI de cada proceso
-        pi = 0.0;
-        MPI_FlattreeColectiva(&mypi, &pi, numprocs, myid);
-
-        if (myid == 0) {
+        double resultado;
+        MPI_FlattreeColectiva(&sum, &resultado, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (rank == 0) {
+            pi = h * resultado;
             printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
         }
     }
-
     MPI_Finalize();
-    return 0;
+}
+
+void MPI_FlattreeColectiva(void * buff, void * recvbuff, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm) {
+    int size, myrank, i;
+    MPI_Comm_size(comm, &size);
+    MPI_Comm_rank(comm, &myrank);
+
+    if(myrank != root){
+        MPI_Send(buff, count, datatype, root, 0, comm);
+    }
+    else{
+        double aux;
+        *(double*)recvbuff = *(double*)buff;
+        for (i = 1; i < size; i++) {
+            MPI_Recv(&aux, count, datatype, i, 0, comm, MPI_STATUS_IGNORE);
+            *(double*)recvbuff += aux;
+        }
+    }
+}
+
+void MPI_BinomialColectiva(void* buff, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
+    int size, myrank, i;
+    MPI_Comm_size(comm, &size);
+    MPI_Comm_rank(comm, &myrank);
+    if(myrank!=root){
+        MPI_Recv(buff, 1, datatype, MPI_ANY_SOURCE, 0, comm, MPI_STATUS_IGNORE);
+    }
+
+    int offset= pow(2,0);
+
+    for(i=0;(myrank+offset) < size;i++) {
+        offset = pow(2, i);
+        if (myrank < offset) {
+            int destino = myrank + offset;
+            if (destino < size) {
+                MPI_Send(buff, 1, datatype, destino, 0, comm);
+            }
+        }
+    }
 }
